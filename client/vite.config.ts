a58174d929ff0d5dfc39fb1423fd9724d182fdde
Mahmoud S. Mohammed <77681600+TheDeadSlayer@users.noreply.github.com>
2025-01-23 02:51:51 -0500

    Updated config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Changed from '/' to relative path
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  },
-  base: '/'
+    emptyOutDir: true,
+    assetsDir: 'assets'
+  }
 })
